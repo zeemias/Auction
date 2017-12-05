e1609279aee63303c0107c76c4978359044fa857
Fauzat <mpr.pass@yandex.ru>
2017-12-05 17:54:53 +0300

    Update

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -70,15 +70,11 @@ namespace Auction.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
+        [Display(Name = "Coints")]
+        public int Coints { get; set; }
+
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 
     public class ResetPasswordViewModel
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -3,12 +3,15 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace Auction.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int Coints { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -18,8 +21,35 @@ namespace Auction.Models
         }
     }
 
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DefaultBet { get; set; }
+        public int Step { get; set; }
+        public int LastBet { get; set; }
+        public string LastUserId { get; set; }
+        public DateTime LastBetTime { get; set; }
+        public DateTime TimeOut { get; set; } 
+    }
+
+    public class Story
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public string User { get; set; }
+        public string UserId { get; set; }
+        public int LastBet { get; set; }
+        public int NewBet { get; set; }
+        public DateTime Time { get; set; }
+    }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Story> Stories { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
